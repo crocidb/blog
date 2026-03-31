ef83bbfcfccf9782ca2ecc2a566ed23e380c8ea4
Bruno Croci <bruno@croci.me>
2026-03-31 23:48:11 +0200

    adding obsidian-to-blog tool to the nix-shell

--- shell.nix
+++ shell.nix
@@ -4,5 +4,10 @@ pkgs.mkShell {
   buildInputs = [
     pkgs.hugo
     pkgs.git
+    pkgs.uv
   ];
+
+  shellHook = ''
+    uv tool install git+https://github.com/crocidb/obsidian-to-blog.git
+  '';
 }
